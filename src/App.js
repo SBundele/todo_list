48fc7b93d689a8837ade2c07684443243e28fc49
Sourabh Bundele <sbundele0318@gmail.com>
2025-01-31 15:05:22 +0530

    added add-todo feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo.trim() === '') return;
+
+    setTodos([{id: `${todo}-${Date.now()}`, todo}, ...todos]);
+    setTodo('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>ToDo List</h1>
+        <form className='todoForm' action="" onSubmit={handleSubmit}>
+          <input type="text" placeholder='Add a task' value={todo} onChange={(e) => setTodo(e.target.value)} />
+          <button type='submit'>Add ToDo</button>
+        </form>
+
+        <ul className="allTodos">
+          {
+            todos.map((todo) => (
+              <li key={todo.id}>
+                <span>{todo.todo}</span>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
